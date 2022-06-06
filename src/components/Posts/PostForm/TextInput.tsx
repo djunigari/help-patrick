0c12b1488d52fb5952fe0aad5869bc6b3f4c63d5
Alexandre Djun Igari <djun_igari@hotmail.com>
2022-06-06 10:51:22 +0900

    Adding Categori and othres fields from post

--- src/components/Posts/PostForm/TextInput.tsx
+++ src/components/Posts/PostForm/TextInput.tsx
@@ -1,6 +1,6 @@
 import { Contact, Post } from "@atoms/postsAtom"
-import { Button, Flex, Input, InputGroup, InputLeftElement, Stack, Textarea } from "@chakra-ui/react"
-import { ChangeEvent } from "react"
+import { Button, Flex, Grid, GridItem, Input, InputGroup, InputLeftElement, InputRightElement, Stack, Textarea } from "@chakra-ui/react"
+import { ChangeEvent, useState } from "react"
 import { BsFacebook, BsWhatsapp } from "react-icons/bs"
 import { FaFacebookSquare } from "react-icons/fa"
 import { SiInstagram } from "react-icons/si"
@@ -12,8 +12,80 @@ interface TextInputProps {
 }
 
 function TextInput({ post, setPost }: TextInputProps) {
+    const [tag, setTag] = useState<string>('')
     return (
         <Stack spacing={3} width='100%'>
+            <Stack direction={{ base: 'column', md: 'row' }} spacing={3}>
+                <Input
+                    name='categoria'
+                    value={post.title}
+                    onChange={(event) => setPost({ ...post, categoria: event.target.value })}
+                    fontSize='10pt'
+                    borderRadius={4}
+                    placeholder='Categoria'
+                    _placeholder={{ color: 'gray.500' }}
+                    _hover={{
+                        outline: 'none',
+                        bg: 'white',
+                        border: '1px solid',
+                        borderColor: 'black'
+                    }}
+                />
+                <Input
+                    name='subcategoria'
+                    value={post.title}
+                    onChange={(event) => setPost({ ...post, subcategoria: event.target.value })}
+                    fontSize='10pt'
+                    borderRadius={4}
+                    placeholder='SubCategoria'
+                    _placeholder={{ color: 'gray.500' }}
+                    _hover={{
+                        outline: 'none',
+                        bg: 'white',
+                        border: '1px solid',
+                        borderColor: 'black'
+                    }}
+                />
+                <InputGroup size='md'>
+                    <Input
+                        name='tag'
+                        value={tag}
+                        onChange={(event) => setTag(event.target.value)}
+                        fontSize='10pt'
+                        borderRadius={4}
+                        placeholder='Adicionar #Tag'
+                        _placeholder={{ color: 'gray.500' }}
+                        _hover={{
+                            outline: 'none',
+                            bg: 'white',
+                            border: '1px solid',
+                            borderColor: 'black'
+                        }}
+                    />
+                    <InputRightElement width='4.5rem'>
+                        <Button h='1.75rem' size='sm' onClick={() => setPost({ ...post, tags: [...(post.tags || []), tag] })}>
+                            Add
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+
+
+            </Stack>
+            <Input
+                name='tags'
+                value={post.tags}
+                readOnly
+                fontSize='10pt'
+                borderRadius={4}
+                placeholder='Title'
+                _placeholder={{ color: 'gray.500' }}
+                _hover={{
+                    outline: 'none',
+                    bg: 'white',
+                    border: '1px solid',
+                    borderColor: 'black'
+                }}
+            />
             <Input
                 name='title'
                 value={post.title}
